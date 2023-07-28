f808daa57220224b81b3fa166a59ca7e3adc8305
sachinsharma263 <sachinsharma263@gmail.com>
2023-07-28 16:18:30 +0530

    udated main method

--- JavaSessions8/src/com/qa/javasessions/MainMethod.java
+++ JavaSessions8/src/com/qa/javasessions/MainMethod.java
@@ -3,8 +3,7 @@ package com.qa.javasessions;
 public class MainMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("main method");
 	}
 
 }
